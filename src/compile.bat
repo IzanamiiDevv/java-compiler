f46eb6bcb1f7a5fc6d0706f391f38b182b7fbcac
IzanamiiDevv <rafaeloli596@gmail.com>
2024-11-12 23:48:30 +0800

    update code

--- src/compile.bat
+++ src/compile.bat
@@ -1,5 +1,5 @@
 @echo off
-
+::COMPILE::
 javac -d ./../bin ./../%~1.java
-
 java -cp ./../bin %~1
+::END::
--- src/java-compile.cpp
+++ src/java-compile.cpp
@@ -2,30 +2,74 @@
 #include <cstdlib>
 #include <cstring>
 #include <string>
+#include <stdint.h>
+#include <map>
 
-struct Java {
-    bool wasJava;
-    std::string fname;
+#define CREDITS "IzanamiiDevv"
+
+namespace Java {
+    static void tokenize(std::string &buffer);
+    static bool isJava(const char* buffer);
+    static void compile(const std::string &fname);
 };
 
-Java isJava(const char* buff);
+const std::map<std::string, uint8_t> create_action();
+int run_action(const std::string cmd);
+
+const std::map<std::string, uint8_t> action = create_action();
 
 int main(int argc, char* argv[]) {
-    Java result = isJava(argv[1]);
-    if (result.wasJava) {
-        std::string command = "compile " + result.fname;
-        system(command.c_str());
-    }
+    if(!Java::isJava(argv[1])) return run_action(argv[1]);
+    
+    std::string jvfile = std::string(argv[1]);
+    Java::tokenize(jvfile);
+    Java::compile(jvfile);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-Java isJava(const char* buff) {
-    Java result = {false, ""};
-    size_t len = strlen(buff);
-    if (len > 5 && strcmp(buff + len - 5, ".java") == 0) {
-        result.wasJava = true;
-        result.fname = std::string(buff, len - 5);
+void Java::tokenize(std::string &buffer) {
+    uint8_t len = buffer.length();
+    buffer = buffer.substr(0, len - 5);
+}
+
+bool Java::isJava(const char* buffer) {
+    uint8_t len = strlen(buffer);
+    if(!(len > 5 && strcmp(buffer + len - 5, ".java") == 0)) return false;
+    return true;
+}
+
+void Java::compile(const std::string &fname) {
+    std::cout << system(("compile " + fname).c_str()) << std::endl;
+}
+
+const std::map<std::string, uint8_t> create_action() {
+    std::map<std::string, uint8_t> buffer;
+    buffer["-version"] = 0;
+    buffer["-credits"] = 1;
+    buffer["-help"] = 2;
+
+    return buffer;
+}
+
+int run_action(const std::string cmd) {
+    try {
+        switch (action.at(cmd)) {
+        case 0:
+            std::cout << "Test 1" << std::endl;
+            break;
+        case 1:
+            std::cout << "Test 2" << std::endl;
+            break;
+        case 2:
+            std::cout << "Test 3" << std::endl;
+            break;
+        }
+    } catch(const std::exception& e) {
+        std::cerr << "Error: action was not defined" << std::endl;
+        std::cout << "Program Exiting with an Error" << std::endl;
+        return EXIT_FAILURE;
     }
-    return result;
+    
+    return EXIT_SUCCESS;
 }
